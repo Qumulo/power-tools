cc0ea766ae55f77efb4b2d7c30616d4b98cb8f49
Tommy Unger <tommyunger@gmail.com>
2019-01-21 21:59:29 -0800

    qumulo api tree walk

--- walk-tree.py
+++ walk-tree.py
@@ -1,148 +1,136 @@
 import os
-import re
 import sys
-import ujson
-import requests
-import urllib
+import glob
 import time
-import random
 import argparse
+import pprint
+import datetime
 import multiprocessing
+if sys.version_info[0] < 3:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
 from qumulo.rest_client import RestClient
 
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-requests.packages.urllib3.disable_warnings()
 
-# paralleliziation across nodes didn't help as this was client-bound
-CLUSTER = ""
-API_USER = ""
-API_PASSWORD = ""
-START_DIR = "/"
-CLUSTER_IPS = []
-OUT_FW = open("file-tree-output.txt", "wb")
+class Gvars:
+    def __init__(self, h, u, p):
+        self.QHOST = h
+        self.QUSER = u
+        self.QPASS = p
+        self.the_queue = multiprocessing.Queue()
+        self.the_queue_len = multiprocessing.Value('i', 0)
+        self.done_queue_len = multiprocessing.Value('i', 0)
 
 
-def worker(q, val, lock, inode_count, dir_count, cluster_ips, api_user, api_pass):
-    ip = random.choice(cluster_ips)
-    rc = RestClient(ip, 8000)
-    creds = rc.login(api_user, api_pass)
-    ses = requests.Session()
-    headers = {"Authorization": "Bearer %s" % str(creds.bearer_token)}
-    ses.headers.update(headers)
+def log(msg):
+    t = datetime.datetime.utcnow()
+    print("%s - %s" % (t.strftime('%Y-%m-%dT%H:%M:%SZ'), msg))
 
-    while True:
-        item = q.get(True)
-        ret_data = read_dir(ip, ses, q, val, lock, item)
-        with lock:
-            val.value -= 1
-            inode_count.value += ret_data["inode_count"]
-            dir_count.value += 1
-            OUT_FW.write('\n'.join(ret_data["rows"]))
-            OUT_FW.write('\n')
-            OUT_FW.flush()
-        time.sleep(0.01)
 
-def read_dir(ip, ses, q, val, lock, path):
-    inode_count = 0
-    url = 'https://%s:8000/v1/files/%s/entries/?limit=1000000' % (ip, urllib.quote_plus(path))
-    resp = ses.get(url, verify=False)
-    obj = ujson.loads(resp.text)
-    rows = []
-    while 'files' in obj:
-        items = obj['files']
-        for d in items:
-            inode_count += 1
-            # Attributes that might be interesting:
-            #                  id: 575005817
-            #                name: geos
-            #                path: /geos/
-            #                size: 512
-            #               owner: 12884901888
-            #               group: 17179869184
-            #                type: FS_FILE_TYPE_DIRECTORY
-            # symlink_target_type: FS_FILE_TYPE_UNKNOWN
-            #       creation_time: 2017-11-17T04:38:22.19858697Z
-            #   modification_time: 2017-11-17T05:13:59.59775965Z
-            #         change_time: 2017-11-17T05:13:59.59775965Z
-            #                mode: 0755
-            #       owner_details: {u'id_type': u'NFS_UID', u'id_value': u'0'}
-            #       group_details: {u'id_type': u'NFS_GID', u'id_value': u'0'}
-            # extended_attributes: {u'read_only': False, u'temporary': False, u'system': False, u'compressed': False, u'not_content_indexed': False, u'hidden': False, u'archive': False}
-            #              blocks: 1
-            #          metablocks: 1
-            #          datablocks: 0
-            #           num_links: 2
-            #         child_count: 1
-            row = "%(path)s\t%(size)s" % d
-            rows.append(row.encode("UTF-8"))
-            if d['type'] == "FS_FILE_TYPE_DIRECTORY":
-                add_to_q(q, val, lock, d['id'])
-        if 'next' in obj['paging']:
-            url = 'https://%s:8000%s' % (ip, obj['paging']['next'])
-            resp = ses.get(url, verify=False)
-            obj = ujson.loads(resp.text)
+def add_to_queue(d):
+    global gvars
+    with gvars.the_queue_len.get_lock():
+        gvars.the_queue_len.value += 1
+    gvars.the_queue.put(d)
+
+
+def list_dir(rc, d, out_file):
+    global gvars
+    next_page = "first"
+    while next_page != "":
+        if next_page == "first":
+            r = rc.fs.read_directory(path=d["path"], page_size=1000)
         else:
-            break
+            r = rc.request("GET", r['paging']['next'])
+        next_page = r['paging']['next']
+        for ent in r["files"]:
+            with gvars.done_queue_len.get_lock():
+                gvars.done_queue_len.value += 1
+            # all potential properties and sample date that are part of the *ent* object to search/track/filter
+            """
+             'path': '/tweets/snow/tweets-snow-2019011902.jsonline',
+             'name': 'tweets-snow-2019011902.jsonline',
+             'change_time': '2019-01-19T10:55:47.591487372Z',
+             'creation_time': '2019-01-19T09:56:01.10147995Z',
+             'modification_time': '2019-01-19T10:55:47.591487372Z',
+             'child_count': 0,
+             'extended_attributes': {'archive': True,
+                                     'compressed': False,
+                                     'hidden': False,
+                                     'not_content_indexed': False,
+                                     'read_only': False,
+                                     'system': False,
+                                     'temporary': False},
+             'id': '11041240942',
+             'group': '17179869184',
+             'group_details': {'id_type': 'NFS_GID', 'id_value': '0'},
+             'owner': '12884901888',
+             'owner_details': {'id_type': 'NFS_UID', 'id_value': '0'},
+             'mode': '0644',
+             'num_links': 1,
+             'size': '34342740',
+             'symlink_target_type': 'FS_FILE_TYPE_UNKNOWN',
+             'type': 'FS_FILE_TYPE_FILE'
+            """
+            # you might set some sort of conditional filter here.
+            # you might even run an api command to set permissions here.
+            out_file.write("%s\t%s\t%s\t%s\n" % (d["path"], ent["name"], ent["size"], ent["type"]))
+            if ent["type"] == "FS_FILE_TYPE_DIRECTORY" and int(ent["child_count"]) > 0:
+                add_to_queue({"path": d["path"] + ent["name"] + "/", "max_depth": d["max_depth"]})
 
-    return {"inode_count": inode_count, "rows": rows}
+
+def worker_main():
+    global gvars
+    proc = multiprocessing.current_process()
+    rc = RestClient(gvars.QHOST, 8000)
+    rc.login(gvars.QUSER, gvars.QPASS)
+    out_file = open("out-%s.txt" % proc.pid, "w")
+    while True:
+        item = gvars.the_queue.get(True)
+        list_dir(rc, item, out_file)
+        out_file.flush()
+        with gvars.the_queue_len.get_lock():
+            gvars.the_queue_len.value -= 1
 
 
-def add_to_q(q, val, lock, item):
-    q.put(item)
-    with lock:
-        val.value += 1
+def walk_tree(QHOST, QUSER, QPASS, start_path):
+    global gvars
+    if start_path[-1] != "/":
+        start_path = start_path + "/"
+    log("Tree walk on Qumulo cluster %s starting at path %s" % (QHOST, start_path))
+    gvars = Gvars(QHOST, QUSER, QPASS)
+    the_pool = multiprocessing.Pool(16, worker_main)
+    rc = RestClient(gvars.QHOST, 8000)
+    rc.login(gvars.QUSER, gvars.QPASS)
+    root = rc.fs.read_dir_aggregates(path=start_path, max_depth=0)
+    log("Directories to walk: %12s" % "{:,}".format(int(root["total_directories"])))
+    log("      Files to walk: %12s" % "{:,}".format(int(root["total_files"])))
+    add_to_queue({"path": start_path, "max_depth": 5})
+    time.sleep(0.1) # wait a bit for the queue to get build up.
+    wait_count = 0
+    while gvars.the_queue_len.value > 0:
+        wait_count += 1
+        if (wait_count % 50) == 0: # show status every ~5 seconds
+            log("Processed %s entries. Queue length: %s" % (gvars.done_queue_len.value, gvars.the_queue_len.value))
+        time.sleep(0.1)
+    the_pool.terminate()
+    log("Processed %s entries." % gvars.done_queue_len.value)
+    log("Done with tree walk. Combining results")
+    fw = open("file-list.txt", "w")
+    for f in glob.glob('out-*.txt'):
+        fr = open(f, "r")
+        fw.write(fr.read())
+        fr.close()
+        os.remove(f)
+    del gvars
+    log("Results save to file: file-list.txt")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', required=True, help='Qumulo host')
-    parser.add_argument('--user', required=True, help='Qumulo api user')
-    parser.add_argument('--password', required=True, help='Qumulo api password')
-    parser.add_argument('--dir', required=False, help='Starting directory', default=START_DIR)
-
+    usage_msg = "\nExample: python walk-tree.py -s qumulo -p password123 -d /home/\nSpecify -h for list of arguments."
+    parser = argparse.ArgumentParser(description='Recursive parallel tree walk with Qumulo API', usage=usage_msg)
+    parser.add_argument('-s', required=True, help='Qumulo cluster ip/hostname')
+    parser.add_argument('-p', required=True, help='Qumulo api *admin* password')
+    parser.add_argument('-d', required=False, help='Starting directory', default='/')
     args = parser.parse_args()
-    CLUSTER = args.host
-    API_USER = args.user
-    API_PASSWORD = args.password
-
-    start_time = time.time()
-    q = multiprocessing.Queue()
-    q_len = multiprocessing.Value('i', 0)
-    q_lock = multiprocessing.Lock()
-
-    rc = RestClient(CLUSTER, 8000)
-    creds = rc.login(API_USER, API_PASSWORD)
-    for d in rc.cluster.list_nodes():
-        c = rc.network.get_network_status_v2(1, d['id'])
-        if len(c['network_statuses'][0]['floating_addresses']) > 0:
-            CLUSTER_IPS.append(c['network_statuses'][0]['floating_addresses'][0])
-        else:
-            CLUSTER_IPS.append(c['network_statuses'][0]['address'])
-
-    start_time = time.time()
-    inode_count = multiprocessing.Value('i', 0)
-    dir_count = multiprocessing.Value('i', 0)
-    last_inode_count = inode_count.value
-    last_dir_count = dir_count.value 
-
-    pool = multiprocessing.Pool(40, worker, (q, q_len, q_lock, inode_count, dir_count, CLUSTER_IPS, API_USER, API_PASSWORD))
-
-    add_to_q(q, q_len, q_lock, args.dir)
-
-    sleep_time = 8
-    while q_len.value > 0:
-        print("Inodes processed: %10s (%s / sec)   Dirs / sec: %s" % (
-                                                inode_count.value
-                                                , int((inode_count.value - last_inode_count) / sleep_time)
-                                                , int((dir_count.value - last_dir_count) / sleep_time)
-                                                ))
-        sys.stdout.flush()
-        last_inode_count = inode_count.value
-        last_dir_count = dir_count.value
-        time.sleep(sleep_time)
-
-    total_time = time.time() - start_time
-    OUT_FW.close()
-    print("processed %s inodes per second" % (int(inode_count.value / total_time),))
-    print("processed %s dirs per second" % (int(dir_count.value / total_time),))
+    r = walk_tree(args.s, "admin", args.p, args.d)
